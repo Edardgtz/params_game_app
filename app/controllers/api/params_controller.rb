58dbbb48d54b4dee9b02590bc6aa203a5272507f
Eduardo Gutierrez <eduardo.gutierrez643@gmail.com>
2020-11-05 20:55:12 -0600

    added a segments parameter route, controller, and method for guess a number game

--- app/controllers/api/params_controller.rb
+++ app/controllers/api/params_controller.rb
@@ -1,25 +1,33 @@
 class Api::ParamsController < ApplicationController
   def letter_a
-    i = 0
-    while i < params["name"].length
-      if params["name"][i].downcase == 'a'
-        @a_name = "Hey, your name starts with the first letter of the alphabet!"
-      end
-      i += 1
+    @name =  params[:name].upcase
+    if @name[0] == 'A'
+      @message = "Hey, your name starts with the first letter of the alphabet!"
+    else
+      @message = "That's a name!"
     end
-    @params = params["name"]
+
+    # i = 0
+    # while i < params[:name].length
+    #   if params[:name][i].downcase == 'a'
+    #     @a_name = "Hey, your name starts with the first letter of the alphabet!"
+    #   end
+    #   i += 1
+    # end
+
     render "name_game.json.jb"
   end
 
   def guess_a_number
-    @number = params["number"]
-    @my_num = 30
-    if params["number"].to_i < @my_num
-      @guess = "Too low"
-    elsif params["number"].to_i > @my_num
-      @guess = "Too high"
-    elsif params["number"].to_i == @my_num
-      @guess = "You got it!!"
+    @guess = params[:guess].to_i
+    correct_answer = 30
+    
+    if @guess < correct_answer
+      @message = "Too low"
+    elsif @guess > correct_answer
+      @message = "Too high"
+    elsif @guess == correct_answer
+      @message = "You got it!!"
     end
     render "name_game.json.jb"
     
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
+    get "segment_parameter/:guess" => "segment_params#guess_a_number"
     get "/a_game" => "params#letter_a"
     get "/guess_number" => "params#guess_a_number"
   end
